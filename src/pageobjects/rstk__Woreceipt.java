188876e60881c5edb28751460efef9274777e6a5
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-05-13 15:29:29 +0530

    testcases changes

--- src/pageobjects/rstk__Woreceipt.java
+++ src/pageobjects/rstk__Woreceipt.java
@@ -1,6 +1,7 @@
 package pageobjects;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,10 +15,20 @@ import com.provar.core.testapi.annotations.BooleanType;
 import com.provar.core.testapi.annotations.ButtonType;
 import com.provar.core.testapi.annotations.ChoiceListType;
 import com.provar.core.testapi.annotations.FindByLabel;
+import com.provar.core.testapi.annotations.PageWait;
+import com.provar.core.testapi.annotations.PageWaitAfter;
 import com.provar.core.testapi.annotations.SalesforcePage;
+import com.provar.core.testapi.annotations.TestLogger;
 import com.provar.core.testapi.annotations.TextType;
 
-@SalesforcePage(title = "Rstk__ Woreceipt", summary = "", page = "Woreceipt", namespacePrefix = "rstk", object = "rstk__worcpt__c", connection = "QARSF_Admin")
+@SalesforcePage( title="Rstk__ Woreceipt"                                
+               , summary=""
+               , page="Woreceipt"
+               , namespacePrefix="rstk"
+               , object="rstk__worcpt__c"
+               , connection="QARSF_Admin"
+     )   
+
 public class rstk__Woreceipt {
 
 	public WebDriver driver;
@@ -26,6 +37,8 @@ public class rstk__Woreceipt {
 		this.driver = driver;
 	}
 
+	@PageWaitAfter.BackgroundActivity(timeoutSeconds = 60)
+	@PageWait.Field(timeoutSeconds = 10)
 	@BooleanType()
 	@FindBy(xpath = "//label[normalize-space(.)='Sort by Item Number']/ancestor::th/following-sibling::td//input")
 	public WebElement sortByItemNumber;
@@ -94,33 +107,40 @@ public class rstk__Woreceipt {
 	@FindByLabel(label = "Perform Work Order Receipt")
 	public WebElement performWorkOrderReceipt;
 
+	@TestLogger
+	public Logger testLogger;
+
 	public void selectWorkOrder(String WorkOrder) throws InterruptedException {
-		List<WebElement> options = driver.findElements(
-				By.xpath("//label[normalize-space(.)='Work Order']/ancestor::th/following-sibling::td//option"));
-		for (WebElement option : options) {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+
+		String elementLocator = "//label[normalize-space(.)='Work Order']/ancestor::th/following-sibling::td//option";
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(elementLocator)));
+		List<WebElement> workOrderList = driver.findElements(By.xpath(elementLocator));
+
+		for (WebElement option : workOrderList) {
 			if (option.getText().contains(WorkOrder)) {
 				option.click();
 				break;
 			}
 		}
+
 	}
 
 	public void selectSerialNumber(Integer NumberOfSerialTobeSelected) throws InterruptedException {
 
-		Thread.sleep(2000);
-
 		List<WebElement> ele = driver.findElements(By.xpath("//select[contains(@id,'serials')]/option"));
-		// testLogger.info("Size" + ele.size());
+		testLogger.info("Size" + ele.size());
 		Select listbox = new Select(driver.findElement(By.xpath("//select[contains(@id,'serials')]")));
 		listbox.deselectAll();
 
 		for (int i = 1; i <= NumberOfSerialTobeSelected; i++) {
-
+			
 			WebElement element = driver.findElement(By.xpath("//select[contains(@id,'serials')]/option[" + i + "]"));
-			// testLogger.info("Serial Number:" + element.getText());
+			testLogger.info("Serial Number:" + element.getText());
 			element.click();
 
 		}
 
 	}
+
 }
