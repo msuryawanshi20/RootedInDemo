77b4de60348e16405e0d751da94f4f6fdc7b0a1d
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-06-01 17:06:20 +0530

    misc

--- src/pageobjects/rstk__SytxncstView.java
+++ src/pageobjects/rstk__SytxncstView.java
@@ -1,11 +1,11 @@
 package pageobjects;
 
-import java.util.List;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import com.provar.core.testapi.annotations.*;
+import com.provar.core.testapi.annotations.LinkType;
+import com.provar.core.testapi.annotations.SalesforcePage;
+import com.provar.core.testapi.annotations.TextType;
 
 @SalesforcePage( title="Rstk__ Sytxncst View"                                
                , summary=""
